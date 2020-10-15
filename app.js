82b639ae543a591b3affd22c8aebeb0e5d7f7027
KotoJeep <72684364+KotoJeep@users.noreply.github.com>
2020-10-15 18:39:30 +0700

    blogerlass

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-let drivers = ['коля', 'жожо', 'омар'];
-for (let i= drivers.length -1 ; i>=0; i--) {
-    console.log(drivers[i]);
+ /*
+ class Hater {
+     constructor(firstName, lastName) {
+         this.firstName = firstName;
+         this.lastName = lastName;
+     }
+     sayName(){
+         console.log(`Наш хейтер: ${this.firstName} ${this.lastName}`);
+     }
+ }
+
+ 
+let hater1 =  new Hater('norman', 'ozborn');
+hater1.sayName();
+*/
+
+class Bloger{
+    constructor(chanelName, firstName, lastName) {
+        this.chanelName = chanelName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    sayName(){
+        console.log(`Канал: ${this.chanelName} [автор канала: ${this.firstName} ${this.lastName} ]` )
+    }
+
 }
+ let Bloger1 = new Bloger('gamelife', 'Andrey', 'Bahrah');
+ Bloger1.sayName();
